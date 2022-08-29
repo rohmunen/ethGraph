1040bf1e4e3b68f40f908e7c4f10257b660b8411
Daniil Salyn <salyndaniil@gmail.com>
2022-08-29 03:37:09 +0300

    type api response

--- src/api/gas.ts
+++ src/api/gas.ts
@@ -8,6 +8,6 @@ type getGasPricesRes = {
 }
 
 export const getGasPrices = async (setGasPrices: React.Dispatch<React.SetStateAction<IData[]>>) => {
-  const data = await axios.get('https://raw.githubusercontent.com/CryptoRStar/GasPriceTestTask/main/gas_price.json')
+  const data = await axios.get<getGasPricesRes>('https://raw.githubusercontent.com/CryptoRStar/GasPriceTestTask/main/gas_price.json')
   setGasPrices(data.data.ethereum.transactions)
 }
